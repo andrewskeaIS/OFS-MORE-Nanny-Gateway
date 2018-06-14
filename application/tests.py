504941d451e742cdf98a5aea17a8b2998752dc7c
Alice King <alice.king@informed.com>
2018-06-14 17:23:14 +0100

    add public app url to build settings

--- application/tests.py
+++ application/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 
 
 class NannyGatewayTests(TestCase):
+
     def test_get_request_returns_404_for_childminder_app_types(self):
         """
         Test that making a query for an application id that is not associated with a record returns a 404 response code.
--- nanny_gateway/settings/build.py
+++ nanny_gateway/settings/build.py
@@ -6,6 +6,8 @@ TEST_MODE = True
 
 INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
+PUBLIC_APPLICATION_URL = 'http://localhost:8000/nanny-gateway'
+
 # Automatic Django logging at the INFO level (i.e everything the comes to the console when ran locally)
 LOGGING = {
   'version': 1,
