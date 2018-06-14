5d670d6a7b3c0131713322baf489e09d211d5b42
Alice King <alice.king@informed.com>
2018-06-14 17:03:01 +0100

    add test configuration

--- application/tests.py
+++ application/tests.py
@@ -1,3 +1,15 @@
+import os
+
+from django.conf import settings
 from django.test import TestCase
 
-# Create your tests here.
+
+class NannyGatewayTests(TestCase):
+    def test_get_request_returns_404_for_childminder_app_types(self):
+        """
+        Test that making a query for an application id that is not associated with a record returns a 404 response code.
+        """
+        prefix = settings.PUBLIC_APPLICATION_URL
+        response = self.client.get(prefix + '/api/v1/user/?application_type=CHILDMINDER')
+
+        self.assertEqual(response.status_code, 404)
