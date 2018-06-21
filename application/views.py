8221e09cc5c9eaf670a2b8b98e563d079dedfd48
Thomas-95is <thomas.stokes@informed.com>
2018-06-21 16:39:41 +0100

    Make generic BaseViewSet.

--- application/views.py
+++ application/views.py
@@ -3,31 +3,12 @@ from django_filters import rest_framework as filters
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 
-from application.models.nanny_models.application import Application, ApplicationSerializer
+from application.models.nanny_models.application import NannyApplication, NannyApplicationSerializer
+from application.models.nanny_models.childcare_training import ChildcareTraining, ChildcareTrainingSerializer
 
 
-class ApplicationViewSet(viewsets.ModelViewSet):
-    """
-    list:
-    List all current applications stored in the database
-    create:
-    Create a new full application in the database
-    retrieve:
-    List the application with the corresponding primary key (application_id) from the database
-    update:
-    Update all fields in a record with the corresponding primary key (application_id) from the database
-    partial_update:
-    Update any amount of fields in  a record with the corresponding primary key (application_id) from the database
-    destroy:
-    Delete the application with the corresponding primary key (application_id) from the database
-
-    """
-    queryset = Application.objects.all()
-    serializer_class = ApplicationSerializer
+class BaseViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.DjangoFilterBackend,)
-    filter_fields = (
-        'application_id',
-    )
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
@@ -42,3 +23,32 @@ class ApplicationViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
+
+class NannyApplicationViewSet(BaseViewSet):
+    """
+    list:
+    List all current applications stored in the database
+    create:
+    Create a new full application in the database
+    retrieve:
+    List the application with the corresponding primary key (application_id) from the database
+    update:
+    Update all fields in a record with the corresponding primary key (application_id) from the database
+    partial_update:
+    Update any amount of fields in  a record with the corresponding primary key (application_id) from the database
+    destroy:
+    Delete the application with the corresponding primary key (application_id) from the database
+    """
+    queryset = NannyApplication.objects.all()
+    serializer_class = NannyApplicationSerializer
+    filter_fields = (
+        'application_id',
+    )
+
+
+class ChildcareTrainingViewSet(BaseViewSet):
+    queryset = ChildcareTraining.objects.all()
+    serializer_class = ChildcareTrainingSerializer
+    filter_fields = (
+        'application_id',
+    )
